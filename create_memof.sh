1cab011d15d03053ff17be42d2898848a75dc0bb
Konstantina Panagopoulou <151877044+konstantina44panag@users.noreply.github.com>
2024-04-23 18:07:19 +0300

    Update create_memof.sh

--- create_memof.sh
+++ create_memof.sh
@@ -4,3 +4,8 @@ for year in $(seq 1993 1 2006); do
     ssh panagopkonst@memos1.troias.offices.aueb.gr "cd ../../; ls /taq93-23/taq${year}taq${y}*" > memo_files${year}
 done
 
+for year in $(seq 2007 1 2023); do
+    for m in $(seq 1 1 12); do
+    month=$(printf '%02d' $m)
+    ssh panagopkonst@memos1.troias.offices.aueb.gr "cd ../../; ls /taq93-23/taq_msec${year}/m${year}${month}" > memo_files${year}${month}
+done
